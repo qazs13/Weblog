99d98583ff243859f6f8921a1e1e59d04a8a233a
Amr Walid Adel <58469345+qazs13@users.noreply.github.com>
2020-05-26 19:35:27 +0200

    Final Push

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@ document.getElementById('submit').addEventListener("click",function(e){
     var userName = document.getElementById('userName').value;
     var password = document.getElementById('password').value;
 
-    if ((userName == "Karim Walid" && password == "AlAlaminTowersGroup1") || (userName == "Test" && password == "Test"))
+    if ((userName == "Group#1" && password == "KWAMH") || (userName == "Test" && password == "Test"))
     {
         e.preventDefault();
-        alert("Login Successfully");
+        alert("Welcome Mr. Karim Walid");
         window.location.replace("pages/main.html");
     }
 });
